bfca3788e7ae4634555bc55d720dc047e9834b72
mike parks <mbparks@gmail.com>
2022-01-25 13:29:25 -0500

    Create arduino_secrets.h

---
+++ Software/smartercnc_fw/arduino_secrets.h
@@ -0,0 +1,6 @@
+#define SECRET_SSID "PortentaAccessPoint"
+#define SECRET_PASS "123Qwerty"
+
+char   HOST_NAME[] = "maker.ifttt.com";
+String PATH_NAME   = "/trigger/event-name/with/key/your-key"; // change your EVENT-NAME and YOUR-KEY
+String queryString = "?value1=Error";
